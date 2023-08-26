761bc86b524beac9b16675870e01afe1baf38f62
rafia02e <rafiabinterashed01@gmail.com>
2023-08-26 08:16:31 -0700

    changes

--- src/Components/Shared/Footer.jsx
+++ src/Components/Shared/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
     <div className='bg-sky-950 '>
       <svg className='  w-full' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#fff" fill-opacity="1" d="M0,256L0,240C130,224,320,192,480,197.3C600,203,800,245,960,261.3C1120,277,1280,267,1360,261.3L1440,256L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
+        <path fill="#ffff" fill-opacity="1" d="M0,25L0,240C130,224,320,192,480,197.3C600,203,800,245,960,261.3C1120,277,1280,267,1360,261.3L1440,256L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"></path>
       </svg>
       <footer className="footer p-10  text-white ">
         <div>
